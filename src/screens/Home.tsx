9ebfcd0ea4a29b67ab8ee64d10fee6c9fe76d142
Hussein Omar <93482934+hussein-jaban@users.noreply.github.com>
2023-04-05 17:58:04 +0300

    more mocks

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,20 +1,26 @@
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {
+  Button,
+  StyleSheet,
+  //  Text,
+  View,
+} from 'react-native';
 import React from 'react';
 import {HomeProps} from '../types/nav.types';
-import Close2 from '../../assests/icons/close2.svg';
-import Close1 from '../../assests/icons/closeIcon.svg';
+// import Close2 from '../../assests/icons/close2.svg';
+// import Close1 from '../../assests/icons/closeIcon.svg';
+import SlideShow from '../components/SlideShow';
+import {imgs} from '../utils/mocks/movieRes';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 const Home = ({navigation}: HomeProps) => {
   return (
-    <View style={styles.main}>
-      <Text>Home Screen</Text>
-      <Close2 width={50} height={50} />
-      <Close1 width={500} height={500} fill={'red'} />
+    <SafeAreaView style={styles.main}>
+      <SlideShow listImages={imgs} />
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
       />
-    </View>
+    </SafeAreaView>
   );
 };
 
