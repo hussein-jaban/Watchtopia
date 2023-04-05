5d97c120cbcd6c86f9dea0d7dadb0013c095aa43
Hussein Omar <93482934+hussein-jaban@users.noreply.github.com>
2023-04-05 17:57:05 +0300

    testing top tabs

---
+++ src/navigations/topTabs/MyTabs.tsx
@@ -0,0 +1,46 @@
+// import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+
+// const Tab = createMaterialTopTabNavigator();
+
+// function MyTabs() {
+//   return (
+//     <Tab.Navigator>
+//       <Tab.Screen name="Home" component={HomeScreen} />
+//       <Tab.Screen name="Settings" component={SettingsScreen} />
+//     </Tab.Navigator>
+//   );
+// }
+
+import {View, Text} from 'react-native';
+import React from 'react';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
+
+function All() {
+  return (
+    <View>
+      <Text>all Screen</Text>
+      {/* <Button title="Go to Home" onPress={() => navigation.navigate('Home')} /> */}
+    </View>
+  );
+}
+function More() {
+  return (
+    <View>
+      <Text>more Screen</Text>
+      {/* <Button title="Go to Home" onPress={() => navigation.navigate('Home')} /> */}
+    </View>
+  );
+}
+
+const MyTabs = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="all" component={All} />
+      <Tab.Screen name="more" component={More} />
+    </Tab.Navigator>
+  );
+};
+
+export default MyTabs;
