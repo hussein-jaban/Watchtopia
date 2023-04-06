19950a72e2df54e275c35364e8bce32458385f15
Hussein Omar <93482934+hussein-jaban@users.noreply.github.com>
2023-04-06 22:03:42 +0300

    rendered sectionSlider comp

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,32 +4,30 @@ import {HomeProps} from '../types/nav.types';
 // import Close2 from '../../assests/icons/close2.svg';
 // import Close1 from '../../assests/icons/closeIcon.svg';
 import SlideShow from '../components/SlideShow';
-import {imgs} from '../utils/mocks/movieRes';
+import {imgs, res} from '../utils/mocks/movieRes';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import SectionSlider from '../components/SectionSlider';
+import {subGenres} from '../constants/genreList';
+
+const resData = (arr: any) => {
+  return arr.map((item: any) => ({
+    id: item.id,
+    imgUrl: item.poster_path,
+  }));
+};
 
 const Home = ({navigation}: HomeProps) => {
   return (
     <SafeAreaView style={styles.main}>
       <ScrollView>
         <SlideShow listImages={imgs} />
-        <Button
-          title="Go to Details"
-          onPress={() => navigation.navigate('Details')}
-        />
-        <Text style={{color: 'white'}}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias
-          impedit beatae nostrum mollitia, facilis nesciunt culpa vero quam
-          quisquam soluta neque, tenetur tempora suscipit veniam natus laborum
-          et! Quod aliquid excepturi obcaecati dolorum unde sed eligendi vel
-          voluptate quasi minus commodi iusto voluptatem adipisci dolores nam
-          rerum explicabo, nesciunt harum, deleniti magni omnis veritatis esse
-          corporis alias. Sit, odit molestiae atque perferendis voluptates
-          aliquid labore ex libero soluta reiciendis delectus expedita nesciunt
-          iure vitae! Esse labore obcaecati voluptate, corrupti eveniet vitae
-          ipsum tempore quam! Beatae cumque quaerat inventore eius laudantium
-          odit. Similique quo porro error tempora! Aliquam quaerat reprehenderit
-          error.
-        </Text>
+        {res.map((item, i) => (
+          <SectionSlider
+            key={i}
+            text={subGenres[i].name}
+            movies={resData(item)}
+          />
+        ))}
       </ScrollView>
     </SafeAreaView>
   );
@@ -43,6 +41,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#0F0E0E',
-    // backgroundColor: 'black',
   },
 });
