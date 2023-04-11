60601b50571d27c87bb1aadd8c5be5583675922f
Hussein Omar <93482934+hussein-jaban@users.noreply.github.com>
2023-04-11 22:33:17 +0300

    removed unused code in searchSatck

--- src/navigations/stacks/SearchStack.tsx
+++ src/navigations/stacks/SearchStack.tsx
@@ -1,19 +1,9 @@
 import React, {useState} from 'react';
-import {
-  Button,
-  // Image,
-  // StyleSheet,
-  Text,
-  // TouchableOpacity,
-  View,
-} from 'react-native';
+import {Button, Text, View} from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-// import Trend from '../../../assests/icons/trend.svg';
 
 import {SearchProps, SearchStackParamList} from '../../types/nav.types';
-// import MyTabs from '../topTabs/MyTabs';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
-// import BottomComponent from '../../components/BottomComponent';
 import BottomSheetWrapper from '../../components/BottomSheetWrapper';
 
 const Tab = createMaterialTopTabNavigator();
@@ -23,6 +13,7 @@ function All() {
   const [isOpen, setIsOpen] = useState(false);
   const onClose = () => setIsOpen(false);
   return (
+    // eslint-disable-next-line react-native/no-inline-styles
     <View style={{backgroundColor: '#000', flex: 1}}>
       <Text>all Screen</Text>
       <Button title="Open" onPress={() => setIsOpen(!isOpen)} />
@@ -60,60 +51,5 @@ const SearchStack = () => {
     </Stack.Navigator>
   );
 };
-
-// const styles = StyleSheet.create({
-//   main: {
-//     flexDirection: 'row',
-//     justifyContent: 'space-between',
-//     marginTop: 10,
-//     marginRight: 10,
-//   },
-//   lineStyle: {
-//     borderWidth: 0.5,
-//     borderColor: 'grey',
-//     marginTop: 12,
-//     marginBottom: 5,
-//   },
-//   icon: {justifyContent: 'center', alignItems: 'center'},
-//   icons: {flexDirection: 'row', justifyContent: 'space-around', marginTop: 20},
-//   min: {color: 'grey'},
-//   min2: {color: 'grey', marginTop: 7, fontSize: 12},
-//   des: {color: 'white'},
-//   des2: {color: 'white', marginLeft: 7, fontSize: 15, fontWeight: '500'},
-//   title: {
-//     color: 'white',
-//     fontSize: 20,
-//     fontWeight: '700',
-//     lineHeight: 27,
-//     flex: 8,
-//   },
-//   right: {flex: 3, color: 'white'},
-//   iconDiv: {backgroundColor: 'white', borderRadius: 50, height: 30, flex: 1},
-//   iconDivCo: {
-//     backgroundColor: 'white',
-//     borderRadius: 50,
-//     height: 35,
-//     width: 35,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   iconDivCo2: {
-//     backgroundColor: 'white',
-//     borderRadius: 50,
-//     height: 36,
-//     width: 36,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   imageSize: {
-//     width: 150,
-//     height: 150,
-//     borderRadius: 10,
-//     resizeMode: 'contain',
-//     flex: 1,
-//     marginHorizontal: 7,
-//     marginRight: 10,
-//   },
-// });
 
 export default SearchStack;
