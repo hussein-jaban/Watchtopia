5c76574b6096add9ed871896beeaef253c3b4949
Hussein Omar <93482934+hussein-jaban@users.noreply.github.com>
2023-04-12 21:39:37 +0300

    hard code bottom sheet content skeloton

--- src/components/BottomSheetWrapper.tsx
+++ src/components/BottomSheetWrapper.tsx
@@ -1,41 +1,49 @@
 import BottomSheet, {
   BottomSheetBackdrop,
   BottomSheetBackdropProps,
-  BottomSheetProps,
+  // BottomSheetProps,
   BottomSheetView,
   useBottomSheetDynamicSnapPoints,
 } from '@gorhom/bottom-sheet';
 import React, {
-  PropsWithChildren,
+  // PropsWithChildren,
   useCallback,
   useEffect,
-  useMemo,
+  // useMemo,
   useRef,
 } from 'react';
-import {BackHandler, StyleSheet, View} from 'react-native';
+import {
+  BackHandler,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Text,
+  View,
+} from 'react-native';
 import {Portal} from 'react-native-portalize';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {res} from '../utils/mocks/movieRes';
+import Trend from '../../assests/icons/trend.svg';
 
 type Props = {
   onClose?: () => void;
   onOpen?: () => void;
   visible: boolean;
-  minPoint?: number | string;
-  maxPoint?: number | string;
-} & Omit<BottomSheetProps, 'snapPoints'>;
+  selected?: any;
+};
 
 const BottomSheetWrapper = ({
-  children,
   onOpen,
   onClose,
   visible,
-  minPoint,
-  maxPoint,
-  ...rest
-}: PropsWithChildren<Props>) => {
+  selected = res[0][0],
+}: Props) => {
   const {top} = useSafeAreaInsets();
   const bottomSheetRef = useRef<BottomSheet>(null);
 
+  // console.log('====================================');
+  console.log(selected);
+  // console.log('====================================');
   // handle back press events for Android
   useEffect(() => {
     const onBackPress = () => {
@@ -56,15 +64,8 @@ const BottomSheetWrapper = ({
     return () => backHandler.remove();
   }, [onClose, visible]);
 
-  const snapPoints: (string | number)[] = useMemo(
-    () =>
-      maxPoint && minPoint
-        ? [minPoint, maxPoint]
-        : maxPoint
-        ? [maxPoint]
-        : ['CONTENT_HEIGHT'],
-    [maxPoint, minPoint],
-  );
+  const snapPoints = ['50%'];
+  const shortify = (text: string) => `${text?.substring(0, 150)}...`;
 
   const {
     animatedContentHeight,
@@ -111,12 +112,102 @@ const BottomSheetWrapper = ({
         enablePanDownToClose={true}
         animateOnMount
         onChange={index => (index === -1 ? onClose?.() : onOpen?.())}
-        snapPoints={animatedSnapPoints}
-        {...rest}>
+        snapPoints={animatedSnapPoints}>
         <BottomSheetView
           onLayout={handleContentLayout}
           style={styles.container}>
-          {children}
+          {/* {children} */}
+          <View>
+            <View style={styles.main}>
+              <Image
+                style={styles.imageSize}
+                source={{
+                  uri: `https://image.tmdb.org/t/p/w500/${selected.poster_path}`,
+                }}
+              />
+              <View style={styles.right}>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                  }}>
+                  <Text style={styles.title}>{selected?.original_title}</Text>
+                  <TouchableOpacity
+                    style={styles.iconDiv}
+                    // onPress={() => close && close?.()}
+                  >
+                    {/* <AntDesignIcon name="closecircle" size={30} color="grey" /> */}
+                    <Trend width={30} height={30} fill={'#fff'} />
+                  </TouchableOpacity>
+                </View>
+                <View style={{flexDirection: 'row', marginVertical: 10}}>
+                  <Text style={styles.min}>
+                    {selected.release_date?.substring(0, 4)}
+                  </Text>
+                  <Text style={[styles.min, {marginHorizontal: 15}]}>
+                    {selected.adult ? '18+' : '13+'}
+                  </Text>
+                  <Text style={styles.min}>3 seasons</Text>
+                </View>
+                <Text style={styles.des}>{shortify(selected.overview)}</Text>
+              </View>
+            </View>
+            <View style={styles.icons}>
+              <View style={styles.icon}>
+                <View style={styles.iconDivCo}>
+                  {/* <EntypoIcons name="controller-play" size={30} color="grey" /> */}
+                  <Trend width={30} height={30} fill={'#fff'} />
+                </View>
+                <Text style={styles.min2}>Play</Text>
+              </View>
+              <View style={styles.icon}>
+                <View style={styles.iconDivCo2}>
+                  {/* <MaterialCommunityIcons
+                  name="download-circle"
+                  size={37}
+                  color="grey"
+                /> */}
+                  <Trend width={30} height={30} fill={'#fff'} />
+                </View>
+                <Text style={styles.min2}>Download</Text>
+              </View>
+              <View style={styles.icon}>
+                <View style={styles.iconDivCo}>
+                  {/* <AntDesignIcon name="pluscircle" size={30} color="grey" /> */}
+                  <Trend width={30} height={30} fill={'#fff'} />
+                </View>
+                <Text style={styles.min2}>My List</Text>
+              </View>
+              <View style={styles.icon}>
+                {/* <FontAwesomeIcons name="share-alt" size={30} color="white" /> */}
+                <Trend width={30} height={30} fill={'#fff'} />
+                <Text style={styles.min2}>Share</Text>
+              </View>
+            </View>
+            <View style={styles.lineStyle} />
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                margin: 12,
+              }}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                }}>
+                {/* <Ionicons
+                name="information-circle-outline"
+                size={30}
+                color="white"
+              /> */}
+                <Trend width={30} height={30} fill={'#fff'} />
+                <Text style={styles.des2}>Details & More</Text>
+              </View>
+              {/* <AntDesignIcon name="right" size={25} color="white" /> */}
+              <Trend width={30} height={30} fill={'#fff'} />
+            </View>
+          </View>
         </BottomSheetView>
       </BottomSheet>
     </Portal>
@@ -139,7 +230,62 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     backgroundColor: '#D0CECD',
   },
-  background: {backgroundColor: '#FCFCFC'},
+  background: {
+    // backgroundColor: '#FCFCFC',
+    backgroundColor: '#333232',
+  },
+  main: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+    marginRight: 10,
+  },
+  lineStyle: {
+    borderWidth: 0.5,
+    borderColor: 'grey',
+    marginTop: 12,
+    marginBottom: 5,
+  },
+  icon: {justifyContent: 'center', alignItems: 'center'},
+  icons: {flexDirection: 'row', justifyContent: 'space-around', marginTop: 20},
+  min: {color: 'grey'},
+  min2: {color: 'grey', marginTop: 7, fontSize: 12},
+  des: {color: 'white'},
+  des2: {color: 'white', marginLeft: 7, fontSize: 15, fontWeight: '500'},
+  title: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: '700',
+    lineHeight: 27,
+    flex: 8,
+  },
+  right: {flex: 3, color: 'white'},
+  iconDiv: {backgroundColor: 'white', borderRadius: 50, height: 30, flex: 1},
+  iconDivCo: {
+    backgroundColor: 'white',
+    borderRadius: 50,
+    height: 35,
+    width: 35,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  iconDivCo2: {
+    backgroundColor: 'white',
+    borderRadius: 50,
+    height: 36,
+    width: 36,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageSize: {
+    width: 150,
+    height: 150,
+    borderRadius: 10,
+    resizeMode: 'contain',
+    flex: 1,
+    marginHorizontal: 7,
+    marginRight: 10,
+  },
 });
 
 export default BottomSheetWrapper;
