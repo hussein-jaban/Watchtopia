efd9717e63e496f317bff132258e3cc5d2e2befd
Hussein Omar <93482934+hussein-jaban@users.noreply.github.com>
2023-04-24 20:52:10 +0300

    refactor modalComponet

--- src/components/ModalComponent.tsx
+++ src/components/ModalComponent.tsx
@@ -1,12 +1,5 @@
 import React, {PropsWithChildren} from 'react';
-import {
-  Alert,
-  Modal,
-  StyleSheet,
-  Pressable,
-  View,
-  ScrollView,
-} from 'react-native';
+import {Alert, Modal, StyleSheet, Pressable, View} from 'react-native';
 import CloseIcon from '../../assests/icons/close-clean.svg';
 
 type Props = {
@@ -31,16 +24,10 @@ const ModalComponent = ({
       }}>
       <View style={styles.main}>
         <View style={styles.modalView}>
-          <ScrollView
-            style={styles.scroll}
-            contentContainerStyle={styles.moreScroll}
-            showsVerticalScrollIndicator={false}>
-            {children}
-            <View style={styles.emp} />
-          </ScrollView>
+          {children}
           <View style={styles.closeView}>
             <Pressable
-              style={styles.buttonClose2}
+              style={styles.buttonClose}
               onPress={() => setModalVisible(!modalVisible)}>
               <CloseIcon width={50} height={50} fill="#000" />
             </Pressable>
@@ -60,41 +47,16 @@ const styles = StyleSheet.create({
     marginTop: 22,
     backgroundColor: 'rgba(0, 0, 0, 0.855)',
   },
-  emp: {marginBottom: 100},
   modalView: {
     margin: 20,
     borderRadius: 20,
     padding: 35,
     justifyContent: 'center',
     alignItems: 'center',
-    // backgroundColor: 'red',
-
     width: '100%',
     height: '100%',
-  },
-  scroll: {
-    // backgroundColor: 'green',
-    textAlign: 'center',
-    width: '100%',
-    height: '100%',
-  },
-  moreScroll: {
-    // flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonOpen: {
-    backgroundColor: '#F194FF',
   },
   buttonClose: {
-    backgroundColor: '#2196F3',
-  },
-  buttonClose2: {
     backgroundColor: 'white',
     width: 50,
     height: 50,
@@ -108,18 +70,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: 'rgba(0, 0, 0, 0.38)',
-  },
-  textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  modalText: {
-    marginTop: 25,
-    marginBottom: 25,
-    textAlign: 'center',
-    color: '#fffefea1',
-    fontSize: 20,
   },
 });
 
