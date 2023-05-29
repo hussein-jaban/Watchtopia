bffd3ef359142eb264204b92074e2998f1d45d2f
Hussein Omar <93482934+hussein-jaban@users.noreply.github.com>
2023-05-29 19:29:46 +0300

    refactor: BlockSection component

--- src/components/BlockSection.tsx
+++ src/components/BlockSection.tsx
@@ -1,27 +1,37 @@
-import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {Image, StyleSheet, TouchableOpacity, View} from 'react-native';
 import React from 'react';
-// import {res} from '../utils/mocks/movieRes';
 
-const BlockSection = ({datas}: any) => {
-  // const datas = res[0];
+type BlockType = {
+  id: string | null;
+  poster_path: string | null;
+};
 
+type Props = {
+  datas: BlockType[];
+};
+
+const BlockSection = ({datas}: Props) => {
   return (
     <View style={styles.main}>
-      {datas?.map((data: any, i: number) => (
-        <TouchableOpacity key={i}>
-          <Image
-            style={styles.imageSize}
-            source={{
-              uri: `https://image.tmdb.org/t/p/w500/${data.poster_path}`,
-            }}
-          />
-        </TouchableOpacity>
-      ))}
-      <TouchableOpacity>
-        <View style={styles.imageSize}>
-          <Text style={styles.whiteTxt}>More</Text>
-        </View>
-      </TouchableOpacity>
+      <View style={styles.wrapper}>
+        {datas?.map((data: BlockType, i: number) => (
+          <TouchableOpacity key={i}>
+            {data.poster_path ? (
+              <Image
+                style={styles.imageSize}
+                source={{
+                  uri: `https://image.tmdb.org/t/p/w500/${data.poster_path}`,
+                }}
+              />
+            ) : (
+              <Image
+                style={styles.noImageSize}
+                source={require('../../assests/images/noPoster.png')}
+              />
+            )}
+          </TouchableOpacity>
+        ))}
+      </View>
     </View>
   );
 };
@@ -30,18 +40,31 @@ export default BlockSection;
 
 const styles = StyleSheet.create({
   main: {
-    flexDirection: 'row',
-    gap: 5,
-    flexWrap: 'wrap',
     flex: 1,
     justifyContent: 'center',
   },
+  wrapper: {
+    flexDirection: 'row',
+    gap: 5,
+    flexWrap: 'wrap',
+  },
   imageSize: {
-    width: 150,
-    height: 150,
+    width: 160,
+    height: 180,
     borderRadius: 5,
     marginHorizontal: -17,
     resizeMode: 'contain',
+    marginTop: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  noImageSize: {
+    width: 120,
+    height: 180,
+    borderRadius: 5,
+    marginHorizontal: 3,
+    resizeMode: 'cover',
     marginTop: 10,
     flexDirection: 'row',
     alignItems: 'center',
